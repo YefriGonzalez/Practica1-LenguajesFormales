31c3a79d89ebb92cd02793ae8075d4f7f464cb31
Yefri González <yefrig00@gmail.com>
2021-08-04 22:08:32 -0600

    Agregue fondo a mi ventana principal

--- src/practica1lenguajesformales/main.java
+++ src/practica1lenguajesformales/main.java
@@ -15,7 +15,8 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        VentanaFrame ventana=new VentanaFrame();
+        ventana.setVisible(true);
     }
     
 }
